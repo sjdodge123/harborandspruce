adb68f9c54832c645d12fc49f425cb23b90ea698
CADY-DESKTOP\Cady <cady>
2021-05-03 22:31:14 -0400

    added social buttons, font awesome ref, scroll to top

--- assets/js/main.js
+++ assets/js/main.js
@@ -153,3 +153,20 @@
       return document.getElementById( id );
     }
   };
+
+  $(document).ready(function(){
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 90) {
+          $('#back-to-top').fadeIn();
+        } else {
+          $('#back-to-top').fadeOut();
+        }
+      });
+      // scroll body to 0px on click
+      $('#back-to-top').click(function () {
+        $('body,html').animate({
+          scrollTop: 0
+        }, 400);
+        return false;
+      });
+  });
